fe388a1af37d76a6afa5b18dd9bc17f75395fc6b
Aryan9901 <911aaryan@gmail.com>
2023-04-08 15:18:17 +0530

    changes bg color

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 /** @format */
 const imageDiv = document.querySelector("#image");
 const elems = document.querySelectorAll(".elems");
+const main = document.querySelector("#main");
+const txt = document.querySelectorAll("h2");
 elems.forEach(function (elem) {
 	// console.log(img, h, w);
 	elem.addEventListener("mouseenter", function () {
 		let img = elem.getAttribute("data-image");
 		let w = elem.getAttribute("data-width");
 		let h = elem.getAttribute("data-height");
+		let c = elem.getAttribute("data-color");
+		var t = elem.getAttribute("data-text");
 		imageDiv.style.backgroundImage = `url(${img})`;
 		imageDiv.style.width = w;
 		imageDiv.style.height = h;
+		main.style.backgroundColor = c;
+		txt.forEach(function (el) {
+			el.style.color = t;
+		});
 	});
 	document.addEventListener("mousemove", function (dets) {
 		imageDiv.style.left = `${dets.x - 100}px`;
