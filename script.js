caefe77a1bff73ae08833dddb5dd6ba00795999e
Aryan9901 <911aaryan@gmail.com>
2023-04-09 12:37:36 +0530

    added overlay animation

--- script.js
+++ script.js
@@ -18,10 +18,52 @@ elems.forEach(function (elem) {
 		txt.forEach(function (el) {
 			el.style.color = t;
 		});
+		elem.addEventListener("click", function () {
+			// console.log("hii");
+			// imageDiv.style.width = "100%";
+			// imageDiv.style.height = "100%";
+			// imageDiv.style.top = "0%";
+			// imageDiv.style.left = "0%";
+			// imageDiv.style.transform = `rotate(0deg)`;
+			gsap.to("#image", {
+				height: "100%",
+				width: "100%",
+				top: 0,
+				duration: 0.7,
+				left: 0,
+				ease: Expo.easeInOut,
+				rotate: "0deg",
+			});
+		});
 	});
 	document.addEventListener("mousemove", function (dets) {
 		imageDiv.style.left = `${dets.x - 100}px`;
 		imageDiv.style.top = `${dets.y - 140}px`;
-		console.log(dets);
+		// console.log(dets);
 	});
+});
+// cursor change
+var outercursor = document.querySelector("#cursor");
+var innercursor = document.querySelector("#curcent");
+function cursorchange() {
+	main.addEventListener("mousemove", function (det) {
+		outercursor.style.left = `${det.pageX}px`;
+		outercursor.style.top = `${det.pageY}px`;
+	});
+}
+cursorchange();
+
+// overlay animation
+var tl = gsap.timeline({
+	repeat: -1,
+});
+tl.to("#overlay", {
+	left: 0,
+	delay: 1,
+	duration: 1.5,
+	ease: Expo.easeInOut.power2,
+}).to("#overlay", {
+	left: "-100%",
+	duration: 1.5,
+	ease: Expo.easeInOut.power2,
 });
